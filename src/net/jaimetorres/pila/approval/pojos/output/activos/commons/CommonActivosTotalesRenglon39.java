ed441944b48fe92f5b5c27318fe8b3be5bfbcc13
alejoHerrer <32083930+alejoHerrer@users.noreply.github.com>
2019-03-13 18:32:33 -0500

    Add files via upload

---
+++ src/net/jaimetorres/pila/approval/pojos/output/activos/commons/CommonActivosTotalesRenglon39.java
@@ -0,0 +1,30 @@
+package net.jaimetorres.pila.approval.pojos.output.activos.commons;
+
+import net.jaimetorres.pila.approval.pojos.output.commons.CommonOutputRecord;
+
+public class CommonActivosTotalesRenglon39 
+extends CommonOutputRecord{
+
+	private String registroTotalesRenglon39;
+	private String tipoRegistro;
+	private String totalCotizacion;
+	
+	public String getRegistroTotalesRenglon39() {
+		return registroTotalesRenglon39;
+	}
+	public void setRegistroTotalesRenglon39(String registroTotalesRenglon39) {
+		this.registroTotalesRenglon39 = registroTotalesRenglon39;
+	}
+	public String getTipoRegistro() {
+		return tipoRegistro;
+	}
+	public void setTipoRegistro(String tipoRegistro) {
+		this.tipoRegistro = tipoRegistro;
+	}
+	public String getTotalCotizacion() {
+		return totalCotizacion;
+	}
+	public void setTotalCotizacion(String totalCotizacion) {
+		this.totalCotizacion = totalCotizacion;
+	}
+}
