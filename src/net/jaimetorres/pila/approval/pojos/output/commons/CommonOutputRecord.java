96c67e783da02215a0c09abd1ac3dc70407adea2
alejoHerrer <32083930+alejoHerrer@users.noreply.github.com>
2019-03-13 18:36:42 -0500

    Add files via upload

---
+++ src/net/jaimetorres/pila/approval/pojos/output/commons/CommonOutputRecord.java
@@ -0,0 +1,16 @@
+package net.jaimetorres.pila.approval.pojos.output.commons;
+
+public abstract class CommonOutputRecord {
+	
+	private int lineNumber;
+
+	public int getLineNumber() {
+		return lineNumber;
+	}
+
+	public void setLineNumber(int lineNumber) {
+		this.lineNumber = lineNumber;
+	}
+
+
+}
